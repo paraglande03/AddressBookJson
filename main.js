8087166cce0e430bb52c00dbc87d5733622a8317
paraglande03 <paraglande03@gmail.com>
2021-03-26 23:28:27 +0530

    Added: methods for sorting

--- main.js
+++ main.js
@@ -1,9 +1,28 @@
 const utility = require('./utility');
-// utility.addDetails();
-utility.displayContact();
-// utility.findPerson();
-// utility.getcount();
-// utility.deleteContact();
-// utility.sortByName()
-// utility.sortByZip()
-utility.Sort_by_FirstName_City_ZipCode()
+const readline = require('readline-sync');
+
+console.log("Choose option to do operation on addressbook\n 1.Add Details:\n", "2.Display Details:\n", "3.Delete Details:\n", "4.search person:\n", "5.Sorting attributes:\n", "6. get count of all contacts");
+let input = readline.questionInt('Enter your choice->');
+switch (input) {
+    case 1:
+        utility.addDetails()
+        break;
+    case 2:
+        utility.displayContact()
+        break;
+    case 3:
+        utility.deleteContact()
+        break;
+    case 4:
+        utility.findPerson()
+        break;
+    case 5:
+        utility.Sort_by_FirstName_City_ZipCode()
+        break
+    case 6:
+        utility.getcount()
+        break;
+    default:
+        break;
+
+}
